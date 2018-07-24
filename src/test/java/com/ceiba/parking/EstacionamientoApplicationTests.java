4f0a7f85841801a490ff7a9e1202ae5b52e0f876
CEIBA\cesar.beltran <cesar.beltran@ceiba.com.co>
2018-07-24 07:56:33 -0500

    Test Unitario para probar configuracion sonar

--- src/test/java/com/ceiba/parking/EstacionamientoApplicationTests.java
+++ src/test/java/com/ceiba/parking/EstacionamientoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.ceiba.parking;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,14 @@ public class EstacionamientoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void testPruebaDosMasDos() {
+		int a = 2, b = 2;
+		int sumaEsperada = 4;
+		
+		assertEquals(sumaEsperada, a+b);
 	}
 
 }
