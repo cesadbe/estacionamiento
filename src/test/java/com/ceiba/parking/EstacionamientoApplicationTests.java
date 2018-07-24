39dbab2e9d420a9a059c2425a5cfa8810fe9a15f
CEIBA\cesar.beltran <cesar.beltran@ceiba.com.co>
2018-07-24 08:14:46 -0500

    Test Unitario para probar configuracion sonar

--- src/test/java/com/ceiba/parking/EstacionamientoApplicationTests.java
+++ src/test/java/com/ceiba/parking/EstacionamientoApplicationTests.java
@@ -22,5 +22,13 @@ public class EstacionamientoApplicationTests {
 		
 		assertEquals(sumaEsperada, a+b);
 	}
+	
+	@Test
+	public void testPruebaTresMasDos() {
+		int a = 3, b = 2;
+		int sumaEsperada = 5;
+		
+		assertEquals(sumaEsperada, a+b);
+	}	
 
 }
