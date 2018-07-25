114953309b39fcc4fb263807cb5628b3679b7e27
CEIBA\cesar.beltran <cesar.beltran@ceiba.com.co>
2018-07-25 09:17:54 -0500

    Test de integracion para el ingreso

---
+++ src/test/java/com/ceiba/parking/dominio/integracion/IngresoVehiculo.java
@@ -0,0 +1,57 @@
+package com.ceiba.parking.dominio.integracion;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IngresoVehiculo {
+/*
+	@Test
+	public void ingresaVehiculoTipoMoto() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void ingresaVehiculoTipoCarro() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void ingresaVehiculoTipoBicicleta() {
+		fail("Not yet implemented");
+	}
+	
+	
+	
+	
+	@Test
+	public void ingresaMotoConParqueaderoMotosLleno() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void ingresaCarroConParqueaderoCarrosLleno() {
+		fail("Not yet implemented");
+	}
+	
+	
+	
+	
+	@Test
+	public void ingresaVehiculoPlacaABC123Domingo() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void ingresaVehiculoPlacaABC123Lunes() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void ingresaVehiculoPlacaABC123Viernes() {
+		fail("Not yet implemented");
+	}
+
+	
+*/
+}
---
+++ src/test/java/com/ceiba/parking/dominio/integracion/SalidaVehiculo.java
@@ -0,0 +1,14 @@
+package com.ceiba.parking.dominio.integracion;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SalidaVehiculo {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
